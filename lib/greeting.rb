87da9c79b52751521fd80e3994640f6750d4bce8
David Greydanus <davidgreydanus@gmail.com>
2015-10-21 15:45:20 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,9 @@
-# code the #greeting method here!
+#!/usr/bin/env ruby
+require_relative "../lib/greeting.rb"
+
+
+def greeting(name)
+  puts "Hello #{ name }. It's nice to meet you."
+end
+
+#puts greeting("Sally") == "Hello Sally. It's nice to meet you."
