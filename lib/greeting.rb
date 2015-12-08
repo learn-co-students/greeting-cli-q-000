662f062771c7553e544cbd85748503e23a6b077e
TyroneMickens <tymic777@gmail.com>
2015-12-08 15:08:20 +0000

    nitrous-backup at Tue Dec  8 15:08:20 UTC 2015

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,10 @@
-# code the #greeting method here!
+
+def greeting(name = "x")
+  puts "Hello #{name[0]} . It's nice to meet you."
+end
+
+ruby/bin/greet
+puts "Hi! I'm HAL, what's your name?"
+Floyd
+Hello Floyd. It's nice to meet you
+(new-content) greeting-cli
