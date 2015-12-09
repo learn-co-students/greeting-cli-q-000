fa96e29a3ad629bc13d21d57c339da319c2417af
TyroneMickens <tymic777@gmail.com>
2015-12-09 21:33:34 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,10 +1,5 @@
 
-def greeting(name = "x")
-  puts "Hello #{name[0]} . It's nice to meet you."
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
 end
 
-ruby/bin/greet
-puts "Hi! I'm HAL, what's your name?"
-Floyd
-Hello Floyd. It's nice to meet you
-(new-content) greeting-cli
