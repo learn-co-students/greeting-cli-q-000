e94b8a55ac96cf3c04415c4247f057245ba562bd
michellejanosi <michellexj11@gmail.com>
2016-02-16 13:01:32 -0500

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,18 +1,5 @@
 # code the #greeting method here!
-def greeting(name = "Michelle")
-  puts "Hi! I'm HAL, what's your name?"
+def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
 end
 
-greeting
-
-# My original code using the 'gets.strip' method
-# but an unusual error is presented after running test.
-
-# def greeting
-#  puts "Hi! I'm HAL, what's your name?"
-#  name = gets.strip
-#  puts "Hello #{name}. It's nice to meet you."
-# end
-
-# greeting
