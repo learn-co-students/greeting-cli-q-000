d3b7c92613f7c0849ef485470794902b550fcd93
saltillokid11 <saltillokid11@yahoo.com>
2016-03-16 12:06:59 -0400

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,13 @@
 # code the #greeting method here!
+require 'date'
+
+
+
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+  puts "How old are you?"
+end
+
+def answer_age(age)
+  puts "So you were born in #{Date.today-age}"
+end
