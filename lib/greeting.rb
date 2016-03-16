4929aa5e37c07552cfb8194fe9564d93a8958784
saltillokid11 <saltillokid11@yahoo.com>
2016-03-16 12:08:19 -0400

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -5,9 +5,9 @@ require 'date'
 
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
-  puts "How old are you?"
+  puts "What year were you born"
 end
 
-def answer_age(age)
-  puts "So you were born in #{Date.today-age}"
+def answer_age(year)
+  puts "If I remember right #{year} was a good year"
 end
