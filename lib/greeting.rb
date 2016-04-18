887125ee9b1ce3791ddbc2aad7dcc9f09c5bbd2c
Sandy <SVern99@aol.com>
2016-04-18 22:55:37 +0000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,5 @@
 # code the #greeting method here!
 def greeting(name)
-   puts "Hi! What's your name?"
-    name = gets
-  puts "Hello #{name}! It's nice to meet you!"
+  puts "Hello #{name}. It's nice to meet you."
 end
+
