2858dbfb7a4061c293f4e6261a24b105b384a145
Dovi S <dovi04+1@gmail.com>
2016-05-26 20:51:13 +1000

    Done.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,6 @@
 # code the #greeting method here!
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+  puts "Wait a second...! You must be that jerk #{name}, everyone has been talking about!"
+  puts "But I digress..."
+end
